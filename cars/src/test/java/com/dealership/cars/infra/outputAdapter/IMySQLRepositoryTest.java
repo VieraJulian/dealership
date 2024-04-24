54a325eed08c1c35f761d19729bc8ea966ac1cb8
VieraJulian <julianviera2019@hotmail.com>
2024-04-24 10:58:41 -0300

    Tests Repository terminados

--- cars/src/test/java/com/dealership/cars/infra/outputAdapter/IMySQLRepositoryTest.java
+++ cars/src/test/java/com/dealership/cars/infra/outputAdapter/IMySQLRepositoryTest.java
@@ -1,4 +1,37 @@
 package com.dealership.cars.infra.outputAdapter;
 
+import com.dealership.cars.domain.Car;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.jdbc.Sql;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+@SpringBootTest
+@TestPropertySource(properties = {
+        "spring.cloud.config.enabled=false",
+        "eureka.client.enabled=false"
+})
+@Sql("/test-mysql.sql")
 public class IMySQLRepositoryTest {
+
+    @Autowired
+    IMySQLRepository myRepository;
+
+    @Test
+    void testFindAllCars(){
+        int page = 0;
+        int size = 3;
+        Pageable pageable = PageRequest.of(page, size);
+
+        List<Car> carList = myRepository.findAllCars(pageable);
+
+        assertEquals(3, carList.size());
+    }
 }
